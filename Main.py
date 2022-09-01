c33b616ca71b3d09d3561e2b5eb96f779b903a2d
laureltheodore <110010515+laureltheodore@users.noreply.github.com>
2022-09-01 21:35:20 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -27,9 +27,37 @@ class DoublyCircularLinkedList:
 
     def add_at_head(self, data) -> bool:
         # Write code here
+        new_node = Node(data)
+        if self.count > 0:
+            new_node.next = self.head
+            new_node.previous = self.end
+            self.head.previous = new_node
+            self.end.next = new_node
+        else:
+            self.end = new_node
+        self.head = new_node
+        self.count += 1
+        return True
 
     def add_at_index(self, index, data) -> bool:
         # Write code here
+        if index < 0 or index >= self.count:
+            return False
+        if index == 0:
+            return self.add_at_head(data)
+        if index == self.count:
+            return self.add_at_tail(data)
+        
+        new_node = Node(data)
+        curr_node = self.head
+        for i in range(index):
+            curr_node = curr_node.next
+        new_node.previous = curr_node.previous
+        new_node.next = curr_node
+        curr_node.previous.next = new_node
+        curr_node.previous = new_node
+        self.count += 1
+        return True 
 
     def get(self, index) -> int:
         # Write code here
